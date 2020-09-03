527d0522044970e44e0fe18af55fb359c5abef04
Dhanush02 <52448311+Dhanush02@users.noreply.github.com>
2020-09-03 16:50:04 +0530

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,35 +1,79 @@
-console.log("Orange Designer");
-menu = () => {
-  var showNav = document.querySelector(".nav-list");
-  showNav.classList.toggle("show");
-  var a = Array.from(document.querySelectorAll(".show a"));
-  a.forEach((rem) => {
-    rem.addEventListener("click", () => {
-      showNav.classList.remove("show");
+console.log("hii")
+function menu() {
+    var showNav = document.querySelector(".nav");
+    showNav.classList.toggle("show");
+    var a = Array.from(document.querySelectorAll(".show a"));
+    a.forEach((rem) => {
+      rem.addEventListener("click", () => {
+        showNav.classList.remove("show");
+      });
     });
+  };
+  window.addEventListener("scroll", () => {
+    let navbar = document.getElementById("navbar-example2");
+    if (window.pageYOffset > 90) {
+      navbar.style.background = "white";
+      navbar.style.boxShadow = "0 8px 25px 0 rgba(0,0,0,.1)";
+      navbar.classList.add("nav-change");
+    } else {
+      navbar.style.background = "none";
+      navbar.style.boxShadow = "none";
+    }
   });
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
 };
 
-window.addEventListener("scroll", () => {
-  let navbar = document.getElementById("scr-fix");
-  if (window.pageYOffset > 90) {
-    navbar.style.boxShadow = "0 8px 25px 0 rgba(0,0,0,.1)";
-    navbar.classList.add("nav-change");
-  } else {
-    navbar.style.backgroundColor = "white";
-    navbar.style.boxShadow = "none";
-  }
-});
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-// let clientHeight = document.getElementById('home').offsetHeight;
-// console.log(clientHeight);  
-// window.addEventListener('scroll',()=>{
-//     if(window.pageYOffset > 660){
-//         let nav2 = document.getElementById('ul-nav-list2');
-//         nav2.style.color = "orange";
-//     }
-//     else{
-//         let nav2 = document.getElementById('ul-nav-list2');
-//         nav2.style.color = "black"
-//     }
-// })
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
